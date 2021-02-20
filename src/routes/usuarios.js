092bcb471008a9506fe8c6ef06dfe0168eee4571
Edri Alan Angulo ARteaga <edri.alan.94@gmail.com>
2021-02-19 18:41:49 -0600

    modifique la API para que pueda ser consumida por el otro proycto de las practicas de angulae

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -8,6 +8,61 @@ router.get('/consultar', (req,res) => {
     res.json(usuarios);
 });
 
+router.get('/auth/login/:user', (req,res) => {
+    console.log(req);
+
+    const { user } = req.params;
+    var encontrado = null;
+
+    _.each(usuarios, (usr_arr, i) =>
+    {
+        _.each(usr_arr, (usr, j) =>
+        {
+            if(usr.usuario.user == user)
+            {
+                encontrado = usr;
+            }            
+            return false;          
+        });
+       
+       
+    });
+
+    if(encontrado === null)
+    {
+        encontrado = {"sucess": false};
+    }
+
+    res.json(encontrado);
+});
+
+router.post('/autenticacion/login', function(request, response){
+    var param = request.body;   
+    var encontrado = null;
+
+    _.each(usuarios, (usr_arr, i) =>
+    {
+        _.each(usr_arr, (usr, j) =>
+        {
+            console.log(usr.usuario.user + " es igual a " + param.user + "  ? ");
+            console.log(usr.usuario.password + " es igual a " + param.pass + "  ? ");
+            if(usr.usuario.user == param.user && usr.usuario.password == param.pass)
+            {
+                encontrado = usr;
+            }            
+            return false;
+           
+        });           
+    });
+
+    if(encontrado === null)
+    {
+        encontrado = {"sucess": false};
+    }
+
+    response.json(encontrado);    
+});
+
 
 
 module.exports = router; 
